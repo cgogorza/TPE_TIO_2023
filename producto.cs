995b1c94e427f5b2c815dff50ce6e155aaa033d5
Cecilia Irurzun <imcecilia80@yahoo.com.ar>
2023-09-26 20:21:44 -0300

    cambie color

---
+++ producto.cs
@@ -0,0 +1,27 @@
+
+/* Estilos para la página de producto */
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+}
+
+h1 {
+    color: #333;
+}
+/*agrego una imagen*/
+
+p {
+    color: #667;
+}
+
+button {
+    background-color: #007bff;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: #0056b3;
+}
