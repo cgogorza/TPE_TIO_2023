917f900cc125421bf342af225512e55bb8de4b11
Cecilia Irurzun <imcecilia80@yahoo.com.ar>
2023-09-26 20:09:44 -0300

    implemente mejora en el carrito

--- carrito.js
+++ carrito.js
@@ -4,5 +4,5 @@ const addToCartButton = document.querySelector('button');
 addToCartButton.addEventListener('click', () => {
     // Agregar lógica para agregar el producto al carrito aquí
     console.log('Producto agregado al carrito');
-  implemento un carrito de compras;
+  implemento una mejora  en el carrito de compras;
 });
