4a888d083bd7271af6e9fac5a413fb300f0f6d37
Cecilia Irurzun <imcecilia80@yahoo.com.ar>
2023-09-26 19:48:01 -0300

    agregue opcion origen a producto

--- carrito.js
+++ carrito.js
@@ -4,4 +4,5 @@ const addToCartButton = document.querySelector('button');
 addToCartButton.addEventListener('click', () => {
     // Agregar lógica para agregar el producto al carrito aquí
     console.log('Producto agregado al carrito');
+  implemento un carrito de compras;
 });
